45011489a03ef7935635a092699a438ffb4c1e13
Тимур Вахитов <vt@greentechs.pro>
2023-08-16 15:35:26 +0300

    Add filtering with Survived column

--- pages/table.py
+++ pages/table.py
@@ -37,6 +37,14 @@ class ShowTable:
                     left, right = st.columns((1, 20))
                     left.write('↳')
 
+                    if column == 'Survived':
+                        user_radio_input = right.radio('Выжил или погиб в катастрофе' ,('Выжил', 'Погиб'))
+                        data = {'Выжил': 1,'Погиб': 0}
+
+                        self.df = self.df[self.df[column] == data[user_radio_input]]
+
+                        continue
+
                     if is_int64_dtype(self.df[column]):
                         _min = int(self.df[column].min())
                         _max = int(self.df[column].max())
@@ -80,12 +88,13 @@ class ShowTable:
             self.df = self.df[visible_columns]
 
     def show_dataframe(self) -> None:
-        modify = st.checkbox('Добавить фильтры')
         hide_columns = st.checkbox('Убрать лишние колонки')
-
+        
         if hide_columns:
             self.hide_columns()
-
+        
+        modify = st.checkbox('Добавить фильтры')
+        
         if modify:
             self.filter_dataframe()
         
